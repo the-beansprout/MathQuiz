c297a7a70abf8736b4fc3d8a9a2e3b4572530986
breina <breina.adenig123@gmail.com>
2024-12-01 10:34:42 +0800

    created a generate questions function

---
+++ process.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+function GenerateQuestions($numQuestions, $min, $max, $operation){
+    $questions =[];
+    for ($i =0; $i <$numQuestions; $i++){
+        $num1 = rand($min, $max);
+        $num2 = rand($min, $max);
+
+        switch ($operation){
+            case 'addtion':
+                $symbol = '+';
+                $answer = $num1 + $num2;
+                break;
+            case 'subtraction':
+                $symbol = '-';
+                $answer = $num1 - $num2;
+                break;
+            case 'multiplication':
+                $symbol = 'x';
+                $answer = $num1 * $num2;
+                break;
+            default:
+                $symbol ='+';
+                $answer = $num1 + $num2;
+        }
+
+        $choices = generateQuestions($answer);
+        $questions[] =[
+            'question' =>"$num1 $symbol $num2",
+            'answer' => $answer,
+            'choices' => $choices
+        ];
+    }
+}
+?>
