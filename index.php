5e46648c3bb081121bcfe0dcfb68d23c2ea892a0
breina <breina.adenig123@gmail.com>
2024-12-01 12:19:00 +0800

    added a session start in the index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' || isset($_SESSION['questions'])) {
+    header("Location: process.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
