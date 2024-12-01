68bb2c4009673dc42c4259180e5a9471e1605cb6
breina <breina.adenig123@gmail.com>
2024-12-01 11:23:23 +0800

    added the question settings

--- process.php
+++ process.php
@@ -46,4 +46,37 @@ function GenerateChoices($correctAnswer){
         shuffle($choices);
         return $choices;
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['start_quiz'])) {
+    $level = $_POST['level'];
+    $operation = $_POST['operation'];
+    $numQuestions = $_POST['num_questions'];
+    $customMin = $_POST['custom_min'] ?? 1;
+    $customMax = $_POST['custom_max'] ?? 10;
+
+        // Set range based on level
+        switch ($level) {
+            case '1':
+                $min = 1;
+                $max = 10;
+                break;
+            case '2':
+                $min = 11;
+                $max = 100;
+                break;
+            case 'custom':
+                $min = $customMin;
+                $max = $customMax;
+                break;
+            default:
+                $min = 1;
+                $max = 10;
+        }
+
+        $_SESSION['questions'] = generateQuestions($numQuestions, $min, $max, $operation);
+        $_SESSION['current_question'] = 0;
+        $_SESSION['score'] = ['correct' => 0, 'wrong' => 0];
+    }
+    
+
 ?>
